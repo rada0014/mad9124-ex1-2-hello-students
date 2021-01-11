61c15ff416fd5c45edcab1352adfc8b6316b34db
Kareem R <rada0014@algonquinlive.com>
2021-01-12 01:49:38 +0200

    done

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,21 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const students = require('./students.json')
+
+//Testing: Worked!// console.log(students)
+
+function fullNames() {
+    students.forEach((student) =>
+    {
+        console.log(`Hello ` + student.firstName + ' ' + student.lastName)
+    })
+}
+fullNames()
+
+function filtered() {
+    const dStudents = students.filter(student => Number(student.lastName.startsWith("D")));
+    console.log(('Count of last names starting with D is ' + dStudents.length))
+}
+filtered()
